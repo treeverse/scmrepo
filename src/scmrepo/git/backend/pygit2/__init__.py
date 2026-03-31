ac24e77e46f51d7fe693ed968863e09690180ffb
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2026-03-31 09:46:24 +0545

    chore: fix mypy error with new pygit2 release
    See https://github.com/libgit2/pygit2/pull/1457.
    
    Now, pygit2 returns None for valueless boolean entries, instead of failing.
    Dulwich returns "true" in this case, so do the same here to avoid discrepancies between the two backends.
    ```ini
    [some-section "identifier"]
        booleanflag
    ```

--- src/scmrepo/git/backend/pygit2/__init__.py
+++ src/scmrepo/git/backend/pygit2/__init__.py
@@ -118,7 +118,8 @@ class Pygit2Config(Config):
         return ".".join((*section, name))
 
     def get(self, section: tuple[str, ...], name: str) -> str:
-        return self._config[self._key(section, name)]
+        k = self._config[self._key(section, name)]
+        return "true" if k is None else k
 
     def get_bool(self, section: tuple[str, ...], name: str) -> bool:
         from pygit2 import GitError
